6dc7e3e6195c3a88a9e020303d4030d2ee025fa9
kravchenko-rm <kravchenko-rm@ukrposhta.ua>
2017-07-18 23:11:32 +0300

    new methods for MembersPopup page object

--- src/test/java/com/pivvit/phillyzoo/pages/MembersPopup.java
+++ src/test/java/com/pivvit/phillyzoo/pages/MembersPopup.java
@@ -1,10 +1,10 @@
 package com.pivvit.phillyzoo.pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import pivvit.base.BaseFEPage;
+import pivvit.utils.Reporter;
 import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementDecorator;
 import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementLocatorFactory;
 
@@ -16,8 +16,14 @@ public class MembersPopup extends BaseFEPage {
     @FindBy(css = ".campaign-offering-details-container")
     WebElement container;
 
+    @FindBy(css = "input[ng-model$='customerID']")
+    WebElement customerIdInput;
+
     @FindBy(css = "button[ng-click='purchaseStep.search()']")
     WebElement searchButton;
+
+    @FindBy(css = ".form-field .error")
+    WebElement errorMessage;
 
     public MembersPopup() {
         PageFactory.initElements(new HtmlElementDecorator(new HtmlElementLocatorFactory(driver())), this);
@@ -30,15 +36,76 @@ public class MembersPopup extends BaseFEPage {
     public MembersPopup waitForPageLoad() {
         waitForLoad(this);
         driver().switchTo().frame(contentIframe);
-        //WebElement elem = driver().findElement(By.cssSelector(".campaign-offering-details-container"));
+
         waitForVisibility(container, "Waiting for popup to load");
+
         driver().switchTo().defaultContent();
         return this;
     }
 
+    /**
+     * Sets text to customer id input field.
+     * @param customerId string with customer id
+     * @return {@link MembersPopup} page
+     */
+    public MembersPopup inputCustomerId(String customerId) {
+        driver().switchTo().frame(contentIframe);
+
+        Reporter.log("Clearing customer id input.");
+        customerIdInput.clear();
+        Reporter.log("Typing customer id: " + customerId);
+        customerIdInput.sendKeys(customerId);
+
+        driver().switchTo().defaultContent();
+        return this;
+    }
+
+    /**
+     * Clicks search button
+     * @return {@link MembersPopup} page
+     */
+    public MembersPopup clickSearchButton() {
+        driver().switchTo().frame(contentIframe);
+
+        click(searchButton, "Clicking search button.");
+
+        driver().switchTo().defaultContent();
+        return this;
+    }
+
+    /**
+     * Retrieves error message text
+     * @return String which contains error text
+     */
+    public String getErrorMessageText() {
+        driver().switchTo().frame(contentIframe);
+
+        String result = errorMessage.getText();
+
+        driver().switchTo().defaultContent();
+        return result;
+    }
+
+    /**
+     * Checks whether the error message is displayed.
+     * The thing is that the element is always present and visible on the page but
+     * if there's no error, it just has no text.
+     * So the check is performed by verifying if the error text is empty or not.
+     * @return {@code true} in case when the error text is not empty
+     */
+    public boolean isErrorMessageDisplayed() {
+        return !getErrorMessageText().isEmpty();
+    }
+
+    /**
+     * Checks whether the page is loaded by presence of the main element
+     * @return {@code true} in case when the main element is present
+     */
     public boolean isLoaded() {
         driver().switchTo().frame(contentIframe);
+
         boolean result = isElementPresent(container);
+
         driver().switchTo().defaultContent();
         return result;
     }
