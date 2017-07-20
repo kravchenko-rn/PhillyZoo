cb18ecef6d6b80884150edc4be20e3a075864bca
kravchenko-rm <kravchenko-rm@ukrposhta.ua>
2017-07-21 00:25:27 +0300

    new test class; test case WEMT-010 done

---
+++ src/test/java/com/pivvit/phillyzoo/tests/WEMT_010__013.java
@@ -0,0 +1,29 @@
+package com.pivvit.phillyzoo.tests;
+
+import com.pivvit.phillyzoo.actions.Actions;
+import com.pivvit.phillyzoo.pages.HomePage;
+import com.pivvit.phillyzoo.pages.MembersPopup;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pivvit.base.BaseTest;
+
+public class WEMT_010__013 extends BaseTest {
+    @BeforeTest
+    public void init() {
+        new HomePage().open();
+        Actions.navigationActions().openMembersPopup();
+    }
+
+    @Test(testName = "WEMT-010", description = "Verify that error textbox validation appear on the last name textbox when clicking the search button with blank last name and zip code textboxes")
+    public void checkEmptyNameZipCodeSearch() {
+        MembersPopup membersPopup = new MembersPopup()
+                .clickAlternateFieldsLink();
+        Assert.assertTrue(membersPopup.isLastNameInputDisplayed(), "Customer last name inout field is not displayed.");
+        Assert.assertTrue(membersPopup.isZipCodeInputDisplayed(), "Customer zip code input field is not displayed.");
+
+        membersPopup.clickSearchButton();
+        Assert.assertTrue(membersPopup.isLastNameValidationErrorMessageDisplayed(),
+                "Last name validation error is not displayed.");
+    }
+}
