04f1ffc56318bb8e77fb95c5eff068ac12f16071
kravchenko-rm <kravchenko-rm@ukrposhta.ua>
2017-07-23 20:55:47 +0300

    added TODO

--- src/test/java/com/pivvit/phillyzoo/tests/WEMT_014__018.java
+++ src/test/java/com/pivvit/phillyzoo/tests/WEMT_014__018.java
@@ -3,6 +3,7 @@ package com.pivvit.phillyzoo.tests;
 import com.pivvit.phillyzoo.actions.Actions;
 import com.pivvit.phillyzoo.pages.HomePage;
 import com.pivvit.phillyzoo.pages.MembersPopup;
+import com.sun.xml.internal.bind.v2.TODO;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
@@ -29,6 +30,7 @@ public class WEMT_014__018 extends BaseTest {
                 "Disguised information is not displayed after choosing a zip code from dropdown filter.");
     }
 
+    // TODO: dependsOnMethod parameter should be updated and the test should be enabled when the valid phone number is set for this test
     @Test(testName = "WEMT-015", dependsOnMethods = "checkDisguisedInformationOnZipCodeSelect", enabled = false,
             description = "Verify that disguised information is displayed after entering a valid phone number")
     @Parameters("customerPhoneNumber")
