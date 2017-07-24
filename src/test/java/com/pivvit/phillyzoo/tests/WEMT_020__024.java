3068821a7ff216b0c86d0037b1c766a7b1b86e7e
kravchenko-rm <kravchenko-rm@ukrposhta.ua>
2017-07-25 00:10:54 +0300

    WEMT-023 logic update - added @BeforeMethod method

--- src/test/java/com/pivvit/phillyzoo/tests/WEMT_020__024.java
+++ src/test/java/com/pivvit/phillyzoo/tests/WEMT_020__024.java
@@ -3,9 +3,12 @@ package com.pivvit.phillyzoo.tests;
 import com.pivvit.phillyzoo.actions.Actions;
 import com.pivvit.phillyzoo.pages.HomePage;
 import com.pivvit.phillyzoo.pages.MembersPopup;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import pivvit.base.BaseActions;
 import pivvit.base.BaseTest;
 import pivvit.utils.SoftAssert;
 
@@ -16,18 +19,24 @@ public class WEMT_020__024 extends BaseTest {
         Actions.navigationActions().openMembersPopup();
     }
 
-    @Test(testName = "WEMT-023", description = "Verify that error text validation appear on the required text boxes when clicking submit button with all text boxes left blank")
+    @BeforeMethod
     @Parameters({"incompletePhoneNumber", "customerLastName"})
-    public void checkUserInformationBlank(String incompletePhoneNumber, String customerLastName) {
-        SoftAssert softAssert = new SoftAssert();
+    public void refreshAndNavigate(String incompletePhoneNumber, String customerLastName) {
+        BaseActions.frontendInstance().refresh();
 
-        MembersPopup membersPopup = new MembersPopup()
+        new MembersPopup()
                 .clickAlternateFieldsLink()
                 .inputCustomerLastName(customerLastName)
                 .clickSearchButton()
                 .waitTillLoadingIndicatorDisappears()
                 .inputCustomerPhoneNumber(incompletePhoneNumber)
-                .submitPhoneNumber()
+                .submitPhoneNumber();
+    }
+
+    @Test(testName = "WEMT-023", description = "Verify that error text validation appear on the required text boxes when clicking submit button with all text boxes left blank")
+    public void checkUserInformationBlank() {
+        SoftAssert softAssert = new SoftAssert();
+        MembersPopup membersPopup = new MembersPopup()
                 .clickSubmitPurchaseButton();
 
         softAssert.assertTrue(membersPopup.isUserFormFirstNameErrorDisplayed(),
