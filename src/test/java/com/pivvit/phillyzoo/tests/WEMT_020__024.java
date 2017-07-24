8f029d6da6658533dbe3d12edcc4fae8c3c30cc0
kravchenko-rm <kravchenko-rm@ukrposhta.ua>
2017-07-24 23:03:22 +0300

    test case WEMT-23 done

--- src/test/java/com/pivvit/phillyzoo/tests/WEMT_020__024.java
+++ src/test/java/com/pivvit/phillyzoo/tests/WEMT_020__024.java
@@ -1,7 +1,41 @@
 package com.pivvit.phillyzoo.tests;
 
-/**
- * Created by Roman Kravchenko on 24.07.2017.
- */
-public class WEMT_020__024 {
+import com.pivvit.phillyzoo.actions.Actions;
+import com.pivvit.phillyzoo.pages.HomePage;
+import com.pivvit.phillyzoo.pages.MembersPopup;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pivvit.base.BaseTest;
+import pivvit.utils.SoftAssert;
+
+public class WEMT_020__024 extends BaseTest {
+    @BeforeTest
+    public void init() {
+        new HomePage().open();
+        Actions.navigationActions().openMembersPopup();
+    }
+
+    @Test(testName = "WEMT-023", description = "Verify that error text validation appear on the required text boxes when clicking submit button with all text boxes left blank")
+    @Parameters({"incompletePhoneNumber", "customerLastName"})
+    public void checkUserInformationBlank(String incompletePhoneNumber, String customerLastName) {
+        SoftAssert softAssert = new SoftAssert();
+
+        MembersPopup membersPopup = new MembersPopup()
+                .clickAlternateFieldsLink()
+                .inputCustomerLastName(customerLastName)
+                .clickSearchButton()
+                .waitTillLoadingIndicatorDisappears()
+                .inputCustomerPhoneNumber(incompletePhoneNumber)
+                .submitPhoneNumber()
+                .clickSubmitPurchaseButton();
+
+        softAssert.assertTrue(membersPopup.isUserFormFirstNameErrorDisplayed(),
+                "User form first name validation error is not displayed.");
+        softAssert.assertTrue(membersPopup.isUserFormLastNameErrorDisplayed(),
+                "User form last name validation error is not displayed.");
+        softAssert.assertTrue(membersPopup.isUserFormEmailErrorDisplayed(),
+                "User form email validation error is not displayed.");
+        softAssert.assertAll();
+    }
 }
