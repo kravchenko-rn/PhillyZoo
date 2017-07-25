9b67190db2479498d86bdf606a0025b71ba0e059
kravchenko-rm <kravchenko-rm@ukrposhta.ua>
2017-07-25 22:42:08 +0300

    tests logic minor update

--- src/test/java/com/pivvit/phillyzoo/tests/WEMT_020__024.java
+++ src/test/java/com/pivvit/phillyzoo/tests/WEMT_020__024.java
@@ -18,25 +18,17 @@ public class WEMT_020__024 extends BaseTest {
         Actions.navigationActions().openMembersPopup();
     }
 
-    @BeforeMethod
-    @Parameters({"incompletePhoneNumber", "customerLastName"})
-    public void refreshAndNavigate(String incompletePhoneNumber, String customerLastName) {
-        BaseActions.frontendInstance().refresh();
-
-        new MembersPopup()
+    @Test(testName = "WEMT-023", description = "Verify that error text validation appear on the required text boxes when clicking submit button with all text boxes left blank")
+    @Parameters({"incompletePhoneNumber", "customerLastName", "blankFieldErrorText"})
+    public void checkUserInformationBlank(String incompletePhoneNumber, String customerLastName, String expectedErrorText) {
+        SoftAssert softAssert = new SoftAssert();
+        MembersPopup membersPopup = new MembersPopup()
                 .clickAlternateFieldsLink()
                 .inputCustomerLastName(customerLastName)
                 .clickSearchButton()
                 .waitTillLoadingIndicatorDisappears()
                 .inputCustomerPhoneNumber(incompletePhoneNumber)
-                .submitPhoneNumber();
-    }
-
-    @Test(testName = "WEMT-023", description = "Verify that error text validation appear on the required text boxes when clicking submit button with all text boxes left blank")
-    @Parameters("blankFieldErrorText")
-    public void checkUserInformationBlank(String expectedErrorText) {
-        SoftAssert softAssert = new SoftAssert();
-        MembersPopup membersPopup = new MembersPopup()
+                .submitPhoneNumber()
                 .clickSubmitPurchaseButton();
 
         softAssert.assertTrue(membersPopup.isUserFormFirstNameErrorDisplayed(expectedErrorText),
@@ -48,8 +40,9 @@ public class WEMT_020__024 extends BaseTest {
         softAssert.assertAll();
     }
 
-    @Test(testName = "WEMT-021", dependsOnMethods = "checkUserInformationBlank")
-    @Parameters({"invalidEmail", "invalidEmailErrorText"})
+    @Test(testName = "WEMT-021", dependsOnMethods = "checkUserInformationBlank", alwaysRun = true,
+            description = "Verify that error text validation appear on email text box when entering invalid email format")
+    @Parameters({"invalidEmail", "invalidFieldErrorText"})
     public void checkUserFormInvalidEmail(String invalidEmail, String expectedErrorText) {
         SoftAssert softAssert = new SoftAssert();
         MembersPopup membersPopup = new MembersPopup()
