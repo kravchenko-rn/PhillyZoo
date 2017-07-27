f68be8bbb4669b767bed920fbfe5cbeddeebf093
kravchenko-rm <kravchenko-rm@ukrposhta.ua>
2017-07-27 22:11:21 +0300

    test case WEMT-027 done

--- src/test/java/com/pivvit/phillyzoo/tests/WEMT_026__029.java
+++ src/test/java/com/pivvit/phillyzoo/tests/WEMT_026__029.java
@@ -29,7 +29,7 @@ public class WEMT_026__029 extends BaseTest {
     }
 
     @Test(testName = "WEMT-028", dependsOnMethods = "checkNonExistingLastName", alwaysRun = true,
-    description = "Verify that error message appear when no results were returned that match the entered last name and zip code")
+            description = "Verify that error message appear when no results were returned that match the entered last name and zip code")
     @Parameters({"nonExistingLastName", "expectedErrorText", "zipCode"})
     public void checkNonExistingLastNameExistingZipCode(String lastName, String expectedErrorText, String zipCode) {
         MembersPopup membersPopup = new MembersPopup()
@@ -39,5 +39,17 @@ public class WEMT_026__029 extends BaseTest {
                 .waitTillLoadingIndicatorDisappears();
         Assert.assertTrue(membersPopup.isErrorMessageDisplayed(expectedErrorText), "Error message is not displayed.");
         Assert.assertEquals(membersPopup.getErrorMessageText(), expectedErrorText, "Error message is incorrect.");
+    }
+
+    @Test(testName = "WEMT-027", dependsOnMethods = "checkNonExistingLastNameExistingZipCode", alwaysRun = true,
+            description = "Verify that search result returns when searching with valid member last name and zip code")
+    @Parameters({"customerLastName", "zipCode"})
+    public void checkExistingLastNameZipCode(String lastName, String zipCode) {
+        MembersPopup membersPopup = new MembersPopup()
+                .inputCustomerLastName(lastName)
+                .inputCustomerZipCode(zipCode)
+                .clickSearchButton()
+                .waitTillLoadingIndicatorDisappears();
+        Assert.assertTrue(membersPopup.isOptionsResultListDisplayed(), "Search results are not displayed or empty.");
     }
 }
