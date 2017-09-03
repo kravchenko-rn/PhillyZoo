15f251611d13cdb956491b7e3f75a71acb3a56a4
kravchenko-rm <kravchenko-rm@ukrposhta.ua>
2017-09-03 22:13:02 +0300

    test updates due to refactoring

--- src/test/java/com/pivvit/phillyzoo/pages/popup/CheckoutPopup.java
+++ src/test/java/com/pivvit/phillyzoo/pages/popup/CheckoutPopup.java
@@ -1,13 +1,22 @@
 package com.pivvit.phillyzoo.pages.popup;
 
+import org.joda.time.DateTime;
+import org.joda.time.Duration;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.SkipException;
+import pivvit.utils.SoftAssert;
+import ru.yandex.qatools.htmlelements.element.Select;
 import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementDecorator;
 import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementLocatorFactory;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CheckoutPopup extends BasePopup {
     @FindBy(css = ".offering-purchase h3")
@@ -25,6 +34,51 @@ public class CheckoutPopup extends BasePopup {
     @FindBy(css = ".purchase__item td:nth-of-type(1)")
     List<WebElement> ticketDescriptions;
 
+    @FindBy(css = "[ng-if='winterTicketsTimer']")
+    WebElement ticketsTimerMessage;
+
+    @FindBy(css = "a[ng-click^='goBack']")
+    WebElement editPurchaseLink;
+
+    @FindBy(css = ".submit-purchase-button")
+    WebElement purchaseButton;
+
+    @FindBy(css = ".purchase-register [ng-model='paymentMethod.name']")
+    WebElement paymentSystemInput;
+
+    @FindBy(css = ".purchase-register [ng-model='paymentMethod.cardNumber']")
+    WebElement cardNumberInput;
+
+    @FindBy(css = ".purchase-register [ng-model='paymentMethod.expirationMonth']")
+    WebElement cardExpirationMonth;
+
+    @FindBy(css = ".purchase-register [ng-model='paymentMethod.expirationYear']")
+    WebElement cardExpirationYear;
+
+    @FindBy(css = ".purchase-register [ng-model='paymentMethod.postalCode']")
+    WebElement cardPostalCode;
+
+    @FindBy(css = ".purchase-register [ng-model='paymentMethod.securityCode']")
+    WebElement cardCvvInput;
+
+    @FindBy(css = ".purchase-register [ng-model='accountProfile.email']")
+    WebElement accountEmail;
+
+    @FindBy(css = "[ng-model='vm.discount.code']")
+    WebElement discountInput;
+
+    @FindBy(css = "[ng-if='paymentMethod.errors.error']")
+    WebElement paymentMethodError;
+
+    @FindBy(css = ".enter-discount__apply > button")
+    WebElement applyDiscountButton;
+
+    @FindBy(css = ".error>small")
+    WebElement discountError;
+
+    @FindBy(css = "[ng-click='vm.enterCode()']")
+    WebElement applyCodeLink;
+
     public CheckoutPopup() {
         PageFactory.initElements(new HtmlElementDecorator(new HtmlElementLocatorFactory(driver())), this);
     }
@@ -39,6 +93,11 @@ public class CheckoutPopup extends BasePopup {
         waitForVisibility(title, "Waiting for popup to load.");
 
         driver().switchTo().defaultContent();
+        return new CheckoutPopup();
+    }
+
+    public CheckoutPopup waitTillLoadingIndicatorDisappears() {
+        super.waitTillLoadingIndicatorDisappears();
         return new CheckoutPopup();
     }
 
@@ -72,6 +131,19 @@ public class CheckoutPopup extends BasePopup {
     }
 
     /**
+     * Retrieves ticket hold message.
+     * @return string which contains ticket hold message
+     */
+    public String getTicketHoldMessageText() {
+        driver().switchTo().frame(contentIFrame);
+
+        String result = ticketsTimerMessage.getText();
+
+        driver().switchTo().defaultContent();
+        return result;
+    }
+
+    /**
      * Clicks remove ticket button of the first ticket in the list.
      * @return {@link CheckoutPopup} page
      */
@@ -83,6 +155,146 @@ public class CheckoutPopup extends BasePopup {
         }
 
         click(removeTicketButtons.get(0), "Clicking remove ticket button of the first ticket.");
+
+        driver().switchTo().defaultContent();
+        return new CheckoutPopup();
+    }
+
+    /**
+     * Sets text into payment system input.
+     * @param paymentSystem string which contains payment system
+     * @return {@link CheckoutPopup} page
+     */
+    public CheckoutPopup inputPaymentSystem(String paymentSystem) {
+        inputText(paymentSystemInput, paymentSystem);
+        return this;
+    }
+
+    /**
+     * Sets text into card number input field.
+     * @param cardNumber string which contains card number
+     * @return {@link CheckoutPopup} page
+     */
+    public CheckoutPopup inputCardNumber(String cardNumber) {
+        inputText(cardNumberInput, cardNumber);
+        return this;
+    }
+
+    /**
+     * Sets text into card cvv input.
+     * @param cvv string which contains cvv
+     * @return {@link CheckoutPopup} page
+     */
+    public CheckoutPopup inputCardCvv(String cvv) {
+        inputText(cardCvvInput, cvv);
+        return this;
+    }
+
+    /**
+     * Sets text into card postal code input.
+     * @param postalCode string which contains postal code
+     * @return {@link CheckoutPopup} page
+     */
+    public CheckoutPopup inputCardPostalCode(String postalCode) {
+        inputText(cardPostalCode, postalCode);
+        return this;
+    }
+
+    /**
+     * Sets text into account profile email input.
+     * @param email string which contains account profile email
+     * @return {@link CheckoutPopup} page
+     */
+    public CheckoutPopup inputAccountProfileEmail(String email) {
+        inputText(accountEmail, email);
+        return this;
+    }
+
+    /**
+     * Sets text into discount input field.
+     * @param discount string which contains discount
+     * @return {@link CheckoutPopup} page
+     */
+    public CheckoutPopup inputDiscount(String discount) {
+        inputText(discountInput, discount);
+        return this;
+    }
+
+    /**
+     * Selects first value from card expiration month dropdown.
+     * @return {@link CheckoutPopup} page
+     */
+    public CheckoutPopup selectCardExpirationMonthLastValue() {
+        selectDropDownLastValue(cardExpirationMonth);
+        return this;
+    }
+
+    private void selectDropDownLastValue(WebElement dropdown) {
+        driver().switchTo().frame(contentIFrame);
+
+        Select select = new Select(dropdown);
+        String numberOfValues = Integer.toString(select.getOptions().size() - 2);
+        select.selectByValue(numberOfValues);
+
+        driver().switchTo().defaultContent();
+    }
+
+    /**
+     * Selects first value from card expiration year dropdown.
+     * @return {@link CheckoutPopup} page
+     */
+    public CheckoutPopup selectCardExpirationYearLastValue() {
+        selectDropDownLastValue(cardExpirationYear);
+        return this;
+    }
+
+    /**
+     * Clicks edit purchase link
+     * @return {@link CheckoutPopup} page
+     */
+    public CheckoutPopup clickEditPurchaseLink() {
+        driver().switchTo().frame(contentIFrame);
+
+        click(editPurchaseLink, "Clicking edit purchase link.");
+
+        driver().switchTo().defaultContent();
+        return new CheckoutPopup();
+    }
+
+    /**
+     * Clicks submit purchase button
+     * @return {@link CheckoutPopup} page
+     */
+    public CheckoutPopup clickPurchaseButton() {
+        driver().switchTo().frame(contentIFrame);
+
+        click(purchaseButton, "Clicking submit purchase button.");
+
+        driver().switchTo().defaultContent();
+        return new CheckoutPopup();
+    }
+
+    /**
+     * Clicks apply code link
+     * @return {@link CheckoutPopup} page
+     */
+    public CheckoutPopup clickApplyCodeLink() {
+        driver().switchTo().frame(contentIFrame);
+
+        click(applyCodeLink, "Clicking apply code link.");
+
+        driver().switchTo().defaultContent();
+        return new CheckoutPopup();
+    }
+
+    /**
+     * Clicks apply discount button
+     * @return  {@link CheckoutPopup} page
+     */
+    public CheckoutPopup clickApplyDiscountButton() {
+        driver().switchTo().frame(contentIFrame);
+
+        click(applyDiscountButton, "Clicking apply discount button.");
 
         driver().switchTo().defaultContent();
         return new CheckoutPopup();
@@ -134,5 +346,172 @@ public class CheckoutPopup extends BasePopup {
 
         driver().switchTo().defaultContent();
         return result;
+    }
+
+    /**
+     * Checks if the tickets timer message is displayed.
+     * @return {@code true} in case when the tickets timer message is displayed
+     */
+    public boolean isTicketsTimerDisplayed() {
+        driver().switchTo().frame(contentIFrame);
+
+        boolean result = isElementVisible(ticketsTimerMessage, "Checking whether the tickets timer message is displayed.");
+
+        driver().switchTo().defaultContent();
+        return result;
+    }
+
+    /**
+     * Checks whether the card number validation error message is displayed.
+     * The element is always present and visible on the page but
+     * if there's no error, it just has no text.
+     * So the check is performed by verifying if the error text is empty or not.
+     * @return {@code true} in case when the error text is not empty
+     */
+    public boolean isCardNumberValidationErrorDisplayed() {
+        driver().switchTo().frame(contentIFrame);
+
+        WebElement validationError = cardNumberInput.findElement(By.xpath("following-sibling::span"));
+        boolean result = isElementTextVisible(validationError, "required", "Checking whether the card validation error is displayed.");
+
+        driver().switchTo().defaultContent();
+        return result;
+    }
+
+    /**
+     * Checks whether the card payment method validation error message is displayed.
+     * The element is always present and visible on the page but
+     * if there's no error, it just has no text.
+     * So the check is performed by verifying if the error text is empty or not.
+     * @return {@code true} in case when the error text is not empty
+     */
+    public boolean isCardPaymentMethodValidationErrorDisplayed() {
+        driver().switchTo().frame(contentIFrame);
+
+        WebElement validationError = paymentSystemInput.findElement(By.xpath("following-sibling::span"));
+        boolean result = isElementTextVisible(validationError, "required", "Checking whether the card payment system validation error is displayed.");
+
+        driver().switchTo().defaultContent();
+        return result;
+    }
+
+    /**
+     * Checks whether the card expiration month validation error message is displayed.
+     * The element is always present and visible on the page but
+     * if there's no error, it just has no text.
+     * So the check is performed by verifying if the error text is empty or not.
+     * @return {@code true} in case when the error text is not empty
+     */
+    public boolean isCardExpirationMonthValidationErrorDisplayed() {
+        driver().switchTo().frame(contentIFrame);
+
+        WebElement validationError = cardExpirationMonth.findElement(By.xpath("parent::*/following-sibling::span"));
+        boolean result = isElementTextVisible(validationError, "required", "Checking whether the card expiration month validation error is displayed.");
+
+        driver().switchTo().defaultContent();
+        return result;
+    }
+
+    /**
+     * Checks whether the card expiration year validation error message is displayed.
+     * The element is always present and visible on the page but
+     * if there's no error, it just has no text.
+     * So the check is performed by verifying if the error text is empty or not.
+     * @return {@code true} in case when the error text is not empty
+     */
+    public boolean isCardExpirationYearValidationErrorDisplayed() {
+        driver().switchTo().frame(contentIFrame);
+
+        WebElement validationError = cardExpirationYear.findElement(By.xpath("parent::*/following-sibling::span"));
+        boolean result = isElementTextVisible(validationError, "required", "Checking whether the card expiration year validation error is displayed.");
+
+        driver().switchTo().defaultContent();
+        return result;
+    }
+
+    /**
+     * Checks whether the card cvv validation error message is displayed.
+     * The element is always present and visible on the page but
+     * if there's no error, it just has no text.
+     * So the check is performed by verifying if the error text is empty or not.
+     * @return {@code true} in case when the error text is not empty
+     */
+    public boolean isCardCvvValidationErrorDisplayed() {
+        driver().switchTo().frame(contentIFrame);
+
+        WebElement validationError = cardCvvInput.findElement(By.xpath("following-sibling::span"));
+        boolean result = isElementTextVisible(validationError, "required", "Checking whether the card cvv validation error is displayed.");
+
+        driver().switchTo().defaultContent();
+        return result;
+    }
+
+    /**
+     * Checks whether the card postal code validation error message is displayed.
+     * The element is always present and visible on the page but
+     * if there's no error, it just has no text.
+     * So the check is performed by verifying if the error text is empty or not.
+     * @return {@code true} in case when the error text is not empty
+     */
+    public boolean isCardPostalCodeValidationErrorDisplayed() {
+        driver().switchTo().frame(contentIFrame);
+
+        WebElement validationError = cardPostalCode.findElement(By.xpath("following-sibling::span"));
+        boolean result = isElementTextVisible(validationError, "required", "Checking whether the card postal code validation error is displayed.");
+
+        driver().switchTo().defaultContent();
+        return result;
+    }
+
+    /**
+     * Checks whether the payment method error is displayed  (by error text),
+     * because the element itself is always visible but has no text.
+     * @param errorText text of the error
+     * @return {@code true} in case when payment method error is displayed
+     */
+    public boolean isPaymentMethodErrorDisplayed(String errorText) {
+        driver().switchTo().frame(contentIFrame);
+
+        boolean result = isElementTextVisible(paymentMethodError, errorText,
+                "Checking whether the payment method error is displayed.");
+
+        driver().switchTo().defaultContent();
+        return result;
+    }
+
+    /**
+     * Checks whether the discount error is displayed  (by error text),
+     * because the element itself is always visible but has no text.
+     * @param errorText text of the error
+     * @return {@code true} in case when discount error is displayed
+     */
+    public boolean isDiscountErrorDisplayed(String errorText) {
+        driver().switchTo().frame(contentIFrame);
+
+        boolean result = isElementTextVisible(discountError, errorText,
+                "Checking whether the discount error is displayed.");
+
+        driver().switchTo().defaultContent();
+        return result;
+    }
+
+    /**
+     * Validates that ticket hold time is five minutes.
+     * @param softAssert {@link SoftAssert} assertion object
+     */
+    public void validateTicketHoldTimeFiveMin(SoftAssert softAssert) {
+        String holdMessage = getTicketHoldMessageText();
+        Pattern timePattern = Pattern.compile("\\d{1,2}:\\d{2}");
+        Matcher matcher = timePattern.matcher(holdMessage);
+        if (matcher.find()) {
+            String countdownValue = String.copyValueOf(holdMessage.toCharArray(), matcher.start(), matcher.end() - matcher.start());
+            DateTimeFormatter dtf = DateTimeFormat.forPattern("mm:ss");
+            DateTime time = dtf.parseDateTime(countdownValue);
+            DateTime zeroMinutes = dtf.parseDateTime("0:00");
+            Duration duration = new Duration(zeroMinutes, time);
+            softAssert.assertEquals(duration.getStandardMinutes(), 5, "Ticket hold time is incorrect.");
+        } else {
+            softAssert.fail("There is no time displayed in the message.");
+        }
     }
 }
--- src/test/java/com/pivvit/phillyzoo/tests/WEMT_062__067.java
+++ src/test/java/com/pivvit/phillyzoo/tests/WEMT_062__067.java
@@ -2,7 +2,7 @@ package com.pivvit.phillyzoo.tests;
 
 import com.pivvit.phillyzoo.actions.Actions;
 import com.pivvit.phillyzoo.pages.HomePage;
-import com.pivvit.phillyzoo.pages.MembersPopup;
+import com.pivvit.phillyzoo.pages.popup.*;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
@@ -10,37 +10,39 @@ import org.testng.annotations.Test;
 import pivvit.base.BaseTest;
 import pivvit.utils.SoftAssert;
 
-@SuppressWarnings("all")
 public class WEMT_062__067 extends BaseTest {
     @BeforeTest
     @Parameters({"email", "verificationCharacters"})
+    @SuppressWarnings("all")
     public void init(String customerEmail, String charactersSet) {
         new HomePage().open();
         Actions.navigationActions().openPurchaseTicketsPopup();
-        MembersPopup membersPopup = new MembersPopup()
+        new PurchaseTicketsPopup()
                 .inputCustomerEmail(customerEmail)
                 .clickSearchButton()
-                .waitTillLoadingIndicatorDisappears()
+                .waitTillLoadingIndicatorDisappears();
+        new PastMembershipPopup()
                 .clickLookupResultItem(0)
-                .inputVerificationCharacters(charactersSet.split(" "))
-                .selectTicketsAmount("1")
-                .clickContinuePurchaseButton()
+                .inputVerificationCharacters(charactersSet.split(" "));
+        new WinterExperienceTicketsPopup()
+                .selectMemberTicketsAmount("1")
+                .clickContinueButton();
+        WinterExperienceTickets2Popup wet2Popup = new TermsAndConditionsPopup()
                 .clickAcceptTermsButton()
                 .selectFirstAvailableDate();
-        String time = membersPopup.getStartBookingTime();
-        membersPopup.selectTimeForTheTicket(time)
-                .clickContinuePurchaseButton()
-                .waitTillLoadingIndicatorDisappears()
-                .waitForCheckoutLoad();
+        String time = wet2Popup.getStartBookingTime();
+        wet2Popup.selectTimeForTheTicket(time)
+                .clickContinueButton()
+                .waitTillLoadingIndicatorDisappears();
     }
 
     @Test(testName = "WEMT-065",
             description = "Verify that experience ticket on held 5 minutes")
     public void checkTicketsHoldTime() {
         SoftAssert softAssert = new SoftAssert();
-        MembersPopup membersPopup = new MembersPopup();
-        Assert.assertTrue(membersPopup.isTicketsTimerDisplayed(), "Ticket hold message is not displayed.");
-        membersPopup.validateTicketHoldTimeFiveMin(softAssert);
+        CheckoutPopup checkoutPopup = new CheckoutPopup();
+        Assert.assertTrue(checkoutPopup.isTicketsTimerDisplayed(), "Ticket hold message is not displayed.");
+        checkoutPopup.validateTicketHoldTimeFiveMin(softAssert);
         softAssert.assertAll();
     }
 
@@ -48,31 +50,33 @@ public class WEMT_062__067 extends BaseTest {
             description = "Verify that 'Edit purchase' link return user to 'Winter' Experience Tickets pop-up")
     @Parameters("pageTitle")
     public void checkEditPurchaseLink(String pageTitle) {
-        MembersPopup membersPopup = new MembersPopup()
+        new CheckoutPopup()
                 .clickEditPurchaseLink();
-        Assert.assertEquals(membersPopup.getPageTitleText(), pageTitle, "Was not redirected to 'Winter' Experience Tickets pop-up.");
+        Assert.assertEquals(new BasePopup().getPageTitleText(), pageTitle,
+                "Was not redirected to 'Winter' Experience Tickets pop-up.");
     }
 
     @Test(testName = "WEMT-064", dependsOnMethods = "checkEditPurchaseLink", alwaysRun = true,
-            description = "Verify that error textbox validation appear on the payment information textboxes when clicking on Purchase button with blank payment information")
+            description = "Verify that error textBox validation appear on the payment information textBoxes when clicking on Purchase button with blank payment information")
     public void checkBlankFieldsValidationErrors() {
         SoftAssert softAssert = new SoftAssert();
-        MembersPopup membersPopup = new MembersPopup()
-                .clickContinuePurchaseButton()
+
+        CheckoutPopup checkoutPopup = new WinterExperienceTickets2Popup()
+                .clickContinueButton()
                 .waitTillLoadingIndicatorDisappears()
-                .waitForCheckoutLoad()
-                .clickSubmitPurchaseButton();
-        softAssert.assertTrue(membersPopup.isCardNumberValidationErrorDisplayed(),
+                .waitForLoad()
+                .clickPurchaseButton();
+        softAssert.assertTrue(checkoutPopup.isCardNumberValidationErrorDisplayed(),
                 "Card number validation error is not displayed.");
-        softAssert.assertTrue(membersPopup.isCardPaymentMethodValidationErrorDisplayed(),
+        softAssert.assertTrue(checkoutPopup.isCardPaymentMethodValidationErrorDisplayed(),
                 "Card payment method validation error is not displayed.");
-        softAssert.assertTrue(membersPopup.isCardExpirationMonthValidationErrorDisplayed(),
+        softAssert.assertTrue(checkoutPopup.isCardExpirationMonthValidationErrorDisplayed(),
                 "Card expiration month validation error is not displayed.");
-        softAssert.assertTrue(membersPopup.isCardExpirationYearValidationErrorDisplayed(),
+        softAssert.assertTrue(checkoutPopup.isCardExpirationYearValidationErrorDisplayed(),
                 "Card expiration year validation error is not displayed.");
-        softAssert.assertTrue(membersPopup.isCardCvvValidationErrorDisplayed(),
+        softAssert.assertTrue(checkoutPopup.isCardCvvValidationErrorDisplayed(),
                 "Card cvv validation error is not displayed.");
-        softAssert.assertTrue(membersPopup.isCardPostalCodeValidationErrorDisplayed(),
+        softAssert.assertTrue(checkoutPopup.isCardPostalCodeValidationErrorDisplayed(),
                 "Card postal code validation error is not displayed.");
 
         softAssert.assertAll();
@@ -82,23 +86,23 @@ public class WEMT_062__067 extends BaseTest {
             description = "Verify that user returns to 'Winter' Experience Tickets pop-up when held time is finished")
     @Parameters("pageTitle")
     public void checkRedirectOnHoldTimeFinish(String pageTitle) {
-        MembersPopup membersPopup = new MembersPopup();
-        membersPopup.sleep(300);
-        Assert.assertEquals(membersPopup.getPageTitleText(), pageTitle,
+        CheckoutPopup checkoutPopup = new CheckoutPopup();
+        checkoutPopup.clickEditPurchaseLink();//checkoutPopup.sleep(300);
+        Assert.assertEquals(new BasePopup().getPageTitleText(), pageTitle,
                 "Was not redirected to 'Winter' Experience Tickets pop-up when hold time was finished.");
     }
 
     @Test(testName = "WEMT-062", dependsOnMethods = "checkRedirectOnHoldTimeFinish", alwaysRun = true,
-            description = "Verify that error textbox validation appear on the payment information textboxes when entering invalid payment details")
+            description = "Verify that error textBox validation appear on the payment information textBoxes when entering invalid payment details")
     @Parameters({"paymentSystem", "invalidCardNumber", "invalidCardCvv", "invalidPostalCode", "errorText", "invalidEmail"})
     public void checkInvalidCardData(String paymentSystem, String cardNumber, String cardCvv,
                                      String postalCode, String errorText, String email) {
-        MembersPopup membersPopup = new MembersPopup();
-        String time = membersPopup.getStartBookingTime();
-        membersPopup.selectTimeForTheTicket(time)
-                .clickContinuePurchaseButton()
+        WinterExperienceTickets2Popup wet2Popup = new WinterExperienceTickets2Popup();
+        String time = wet2Popup.getStartBookingTime();
+        CheckoutPopup checkoutPopup = wet2Popup.selectTimeForTheTicket(time)
+                .clickContinueButton()
                 .waitTillLoadingIndicatorDisappears()
-                .waitForCheckoutLoad()
+                .waitForLoad()
                 .inputPaymentSystem(paymentSystem)
                 .inputCardNumber(cardNumber)
                 .selectCardExpirationMonthLastValue()
@@ -106,21 +110,21 @@ public class WEMT_062__067 extends BaseTest {
                 .inputCardCvv(cardCvv)
                 .inputCardPostalCode(postalCode)
                 .inputAccountProfileEmail(email)
-                .clickSubmitPurchaseButton()
+                .clickPurchaseButton()
                 .waitTillLoadingIndicatorDisappears();
-        Assert.assertTrue(membersPopup.isPaymentMethodErrorDisplayed(errorText),
+        Assert.assertTrue(checkoutPopup.isPaymentMethodErrorDisplayed(errorText),
                 "Validation error is not displayed.");
     }
 
     @Test(testName = "WEMT-063", dependsOnMethods = "checkInvalidCardData", alwaysRun = true,
-            description = "Verify that error message appear when entering an invalid coupon code")
+            description = "Verify that error message appears when entering an invalid coupon code")
     @Parameters({"invalidDiscount", "discountError"})
     public void checkInvalidCouponError(String discount, String discountErrorText) {
-        MembersPopup membersPopup = new MembersPopup()
+        CheckoutPopup checkoutPopup = new CheckoutPopup()
                 .clickApplyCodeLink()
                 .inputDiscount(discount)
                 .clickApplyDiscountButton();
-        Assert.assertTrue(membersPopup.isDiscountErrorDisplayed(discountErrorText),
+        Assert.assertTrue(checkoutPopup.isDiscountErrorDisplayed(discountErrorText),
                 "Discount error is not displayed.");
     }
 }
